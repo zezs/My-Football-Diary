3497ed5554c5cb79273e858641fff33f313e4d25
zezs <my.version.ctrl@gmail.com>
2024-05-01 01:50:31 +0200

    added flask app factory pattern

--- app.py
+++ app.py
@@ -4,37 +4,42 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-app = Flask(__name__)
-mongo_username = os.getenv("MONGO_USERNAME")
-mongo_password = os.getenv("MONGO_PASSWORD")
-client = MongoClient("mongodb+srv://{}:{}@footall-diary.qe8s4ox.mongodb.net/".format(mongo_username, mongo_password))
-#client = MongoClient("mongodb+srv://username:password@footall-diary.qe8s4ox.mongodb.net/")
-app.db = client.footballdiary
+"""in some situation this file might run multiple times, when we deploty this app, deplyment mechanism can run this app multiple times
+  that would result in creating multiple different mongo clients one for each time the file runs,
+   we solve this problem by using the app factory. 
+   App factory enables deployment mechanism to not create "app" multiple times
+"""
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    mongo_username = os.getenv("MONGO_USERNAME")
+    mongo_password = os.getenv("MONGO_PASSWORD")
+    client = MongoClient("mongodb+srv://{}:{}@footall-diary.qe8s4ox.mongodb.net/".format(mongo_username, mongo_password))
+    #client = MongoClient("mongodb+srv://username:password@footall-diary.qe8s4ox.mongodb.net/")
+    app.db = client.footballdiary
+    # entries = [("Wizard returns", "KDB is back! Klopp said KDB is back from injury and all prem teams are shaking", "2024-02-01")]
 
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        print([e for e in app.db.enries.find({})])
+        if request.method == "POST":
+            entry_title = request.form.get("title") 
+            entry_content = request.form.get("content")
+            date = datetime.datetime.today().strftime("%Y-%m-%d") 
+            # print( entry_title,entry_content, date)
+            # entries.append((entry_title, entry_content, date))
+            app.db.entries.insert_one({"title": entry_title, "content": entry_content, "date": date})
 
+        entries_with_date = [
+                (
+                    entry["title"],
+                    entry["content"],
+                    entry["date"],
+                    datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+                )
+                for entry in app.db.entries.find({})
+        ]
 
-# entries = [("Wizard returns", "KDB is back! Klopp said KDB is back from injury and all prem teams are shaking", "2024-02-01")]
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    print([e for e in app.db.enries.find({})])
-    if request.method == "POST":
-        entry_title = request.form.get("title") 
-        entry_content = request.form.get("content")
-        date = datetime.datetime.today().strftime("%Y-%m-%d") 
-        # print( entry_title,entry_content, date)
-        # entries.append((entry_title, entry_content, date))
-        app.db.entries.insert_one({"title": entry_title, "content": entry_content, "date": date})
-
-    entries_with_date = [
-            (
-                entry["title"],
-                entry["content"],
-                entry["date"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-            )
-            for entry in app.db.entries.find({})
-    ]
-
-    return render_template("index.html", entries = entries_with_date)
+        return render_template("index.html", entries = entries_with_date)
+    
+    return app
